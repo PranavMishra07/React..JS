1864d73b63fbb4725a0dfb6508bc6f63630d53ac
PranavMishra07 <pranav072003@gmail.com>
2025-03-08 18:57:05 +0530

    as

--- router/Router/src/App.jsx
+++ router/Router/src/App.jsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./Home"
 import About from "./About"
-
+import Navbar from "./Navbar"
+import './app.css'
+import Hero from "./Hero"
 function App() {
 
   return (
     <>
+      <Navbar />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/hero" element={<Hero />} />
+
       </Routes>
     </>
   )
---
+++ router/Router/src/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+    return (
+        <>
+            <div className="hero">
+                <div className="imag">
+                    <img src="src/assets/react.svg" alt="" />
+                </div>
+                <div className="data">
+                    <h1>welcome to Hero Section</h1>
+                    <span>Hiii... Developers</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit inventore fugit cumque tempora animi! Adipisci natus eos fuga vel est pariatur quod? Voluptatibus quidem reiciendis ducimus.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Hero
