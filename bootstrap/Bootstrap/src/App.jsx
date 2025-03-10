976d8e6454f6ef2d0a0ace775feaeaf05379fe7a
PranavMishra07 <pranav072003@gmail.com>
2025-03-10 19:10:12 +0530

    ds

---
+++ bootstrap/Bootstrap/src/App.jsx
@@ -0,0 +1,15 @@
+import { Container, Col, Row } from "react-bootstrap"
+
+function App() {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col><h1>Colomn 1</h1></Col>
+          <Col><h1>Colomn 2</h1></Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+export default App
